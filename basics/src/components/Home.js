64436cbbd465931490be72f2b5af323b620ff157
paxonpresley <76454250+paxonpresley@users.noreply.github.com>
2021-11-23 19:47:10 -0600

    prog 1

---
+++ basics/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Stories from './components/Stories'
+
+function Home() {
+    return ( 
+        <div className="Home">
+            <Stories />
+        </div>
+     );
+}
+
+export default Home;
---
+++ basics/src/components/components/Stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Stories() {
+    return ( 
+        <div className="Stories">
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+     );
+}
+
+export default Stories;
